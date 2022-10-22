13190e8c4758b83965d60304e67f788f65c38a91
Woochul Lim <woochul129@gmail.com>
2022-10-22 23:47:52 +0900

    Add all files
    브랜치 정리; 로그인 페이지 아직..

--- components/headers.tsx
+++ components/headers.tsx
@@ -15,21 +15,6 @@ const Header: React.FC = () => {
         <span className="flex title-font font-medium items-center text-gray-900 mb-3 md:mb-0">
           <span className="ml-5 text-xl">SUNY Career Development Center</span>
         </span>
-        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center" />
-        <span className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-          Sign in
-          <svg
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            className="w-4 h-4 ml-1"
-            viewBox="0 0 24 24"
-          >
-            <path d="M5 12h14M12 5l7 7-7 7" />
-          </svg>
-        </span>
       </div>
     </header>
   );
